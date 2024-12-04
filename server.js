5ec14b4e241fdbdf027b343c175e01479f24b197
Arian N. Yamchelo <ariannajafi@icloud.com>
2024-12-04 15:15:03 +1100

    Update server.js file

---
+++ server.js
@@ -0,0 +1,44 @@
+// @file    ./server.js
+
+// Dependencies
+import mongoose from "mongoose";
+import express from "express";
+import cors from "cors";
+import fileUpload from "express-fileupload";
+import dotenv from "dotenv";
+import homeRouter from "./routes/home";
+
+// Database configuration
+dotenv.config();
+const uri = process.env.DB_URI;
+
+mongoose
+  .connect(uri)
+  .then(() => console.log("Connected to DocumentDB."))
+  .catch((error) => {
+    console.error("Connection error:", error.message);
+    console.error("Full error object:", JSON.stringify(error, null, 2));
+    console.error("Stack trace:", error.stack);
+  });
+
+// Express app setup
+const app = express();
+app.use(express.static("assets"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(
+  fileUpload({
+    limits: { fileSize: 50 * 1024 * 1024 },
+  })
+);
+
+// - Homepage route
+app.use("/", homeRouter);
+
+const port = process.env.PORT || 3000;
+
+// Run app (Listen on port) ----------------------------------------------------
+app.listen(port, () => {
+  console.log(`The app is running on port ${port}.`);
+});
